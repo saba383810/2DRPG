b8280570bfdb11a6d00338659a808dc184db77ab
saba383810 <saba383810@gmail.com>
2020-04-21 23:56:03 +0900

    2DRPGを作る

---
+++ 2DRPG/Assets/Scripts/playerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public readonly float SPEED = 0.05f;
+    private Rigidbody2D rigidBody;
+    private Vector2 input;
+
+    void Start()
+    {
+        this.rigidBody = GetComponent<Rigidbody2D>();
+        // 衝突時にobjectを回転させない設定
+        this.rigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void Update()
+    {
+        // 入力を取得
+        input = new Vector2(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        if (input == Vector2.zero)
+        {
+            return;
+        }
+        // 既存のポジションに対して、移動量(vector)を加算する
+        rigidBody.position += input * SPEED;
+    }
+}
